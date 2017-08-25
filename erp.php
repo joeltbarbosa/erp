10cd40e0e605681d7a3aee33e52495fcb977a0a7
joeltbarbosa <joel.tbarbosa@gmail.com>
2017-08-24 22:45:54 -0300

    Commit da atualização  - 22:50h - 24 08 2017

---
+++ erp.php
@@ -0,0 +1,72 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <title>Sistema ERP</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="./css/styles.css" rel="stylesheet">
+</head>
+
+<body>
+<header>
+   <div id="divHeader" style="width: 100%; height: 30px; background-color: #0091ea; margin-bottom: 5px">div</div>
+</header>
+<nav id="naveMenu">
+    <div id='cssmenu'>
+        <ul>
+            <li><a href='#'><span>Home</span></a></li>
+            <li class='active has-sub'><a href='#'><span>Cadastros</span></a>
+                <ul>
+                    <li class='has-sub'><a href='#'><span>Unidade</span></a></li>
+                    <li class='has-sub'><a href='#'><span>Funcionário</span></a></li>
+                    <li class='has-sub'><a href='#'><span>Prestador de Serviços</span></a></li>
+                    <li class='has-sub'><a href='#'><span>Morador</span></a>
+                        <ul>
+                            <li><a href='#'><span>Funcionário de Morador</span></a></li>
+                            <li class='last'><a href='#'><span>Sub Product</span></a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+            <li><a href='#'><span>Sobre</span></a></li>
+            <li class='last'><a href='#'><span>Contato</span></a></li>
+        </ul>
+    </div>
+</nav>
+<aside>
+    asiside
+</aside>
+
+<section id="sectionCorpo">
+    <h3>Texto H3</h3>
+    <article>
+        <p style="text-align: justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus et congue dui.                  Vivamus quis facilisis
+            risus, id condimentum sem. Mauris gravida, mi vitae vulputate mattis, justo mi finibus nibh, nec placerat
+            felis turpis non velit. Proin efficitur augue in rhoncus blandit. Etiam consequat pretium est, ut vehicula
+            neque vestibulum et. Praesent laoreet ipsum non risus rhoncus, a convallis sem efficitur. Curabitur eros
+            mauris, molestie quis tristique ac, pulvinar quis risus. Phasellus consequat, lorem eget porttitor porta,
+            ligula dui sollicitudin urna, in molestie metus massa ut urna. Vivamus erat purus, hendrerit ac porttitor
+            ut, sodales sed mi. Vestibulum efficitur ex vel augue sodales auctor. Donec dictum mi a nunc volutpat
+            dignissim.</p>
+    </article>
+
+    <article>
+        <p style="text-align: justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus et congue dui.                 Vivamus quis facilisis
+            risus, id condimentum sem. Mauris gravida, mi vitae vulputate mattis, justo mi finibus nibh, nec placerat
+            felis turpis non velit. Proin efficitur augue in rhoncus blandit. Etiam consequat pretium est, ut vehicula
+            neque vestibulum et. Praesent laoreet ipsum non risus rhoncus, a convallis sem efficitur. Curabitur eros
+            mauris, molestie quis tristique ac, pulvinar quis risus. Phasellus consequat, lorem eget porttitor porta,
+            ligula dui sollicitudin urna, in molestie metus massa ut urna. Vivamus erat purus, hendrerit ac porttitor
+            ut, sodales sed mi. Vestibulum efficitur ex vel augue sodales auctor. Donec dictum mi a nunc volutpat
+            dignissim.</p>
+    </article>
+</section>
+
+<footer>
+    <p>&copy;Copyright 2017. Todos os direitos reservados.</p>
+</footer>
+</body>
+</html>
